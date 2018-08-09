bb0cb07db1a2d803a5b09c579a73d5d5c6bc92f4
banjac275 <banjac275@gmail.com>
2018-08-09 15:59:17 +0200

    all done but there are some bugs

--- src/validate.js
+++ src/validate.js
@@ -3,118 +3,186 @@ import materialize from './materialize'
 class BanjiValidate {
   constructor(doc) {
     this.doc = doc;
-    this.inputs = null;
+    this.inputs = [];
+    this.ids = null;
+    this.okClass = "input--ok";
+    this.errClass = "input--err"; 
     if(Object.keys(this.doc).length > 0) this.init()
     else console.log("error: doc empty")
   }
   init() {
-    let keys = Object.getOwnPropertyNames(this.doc);
-    //console.log(formInputs)
-    keys.forEach(el => {
-      this.inputs = document.forms[el].querySelectorAll("input");      
-      let ids = Object.getOwnPropertyNames(this.doc[el]);
-      if (this.inputs !== undefined && ids !== undefined) {
-        ids.forEach((id, ind) => {
-          let inputKeys = Object.getOwnPropertyNames(this.doc[el][id]);
-          this.checkKeys(this.inputs[ind], inputKeys, el, id);
+    let keys = Object.getOwnPropertyNames(this.doc.forms);
+    keys.forEach(el => {    
+      this.ids = Object.getOwnPropertyNames(this.doc.forms[el]);
+      if (this.ids !== undefined) {
+        this.ids.forEach((id, ind) => {
+          let input = document.querySelector("#" + this.ids[ind]);
+          this.inputs.push(input);
+          let inputKeys = Object.getOwnPropertyNames(this.doc.forms[el][id]);
+          this.checkKeys(input, inputKeys, el, id);
         })
+        this.keyPressControl(el);
       }
     })
+    let options = Object.getOwnPropertyNames(this.doc.options);
+    if(options.length > 0 || options !== null) {
+      options.forEach(el => {
+        switch(el) {
+          case "customClassOk":
+            if (this.doc.options["customClassOk"].length > 0){
+              this.okClass = this.doc.options["customClassOk"];
+              this.inputs.forEach(inp => {
+                inp.classList.remove(this.okClass);
+                inp.classList.add(this.errClass);
+              })
+            }
+            break;
+          case "customClassError":
+            if (this.doc.options["customClassError"].length > 0){
+              this.errClass = this.doc.options["customClassError"];
+              this.inputs.forEach(inp => {
+                inp.classList.add(this.okClass);
+                inp.classList.remove(this.errClass);
+              })
+            } 
+            break;
+        }
+      })
+    }
   }
   checkKeys(input, keys, element, index) {
-    //console.log(input, keys, element, index)
+    let message = "";
+    let messageCheck = false;
     let inputType = input.type;
     for(let key in keys) {
       switch(keys[key]) {
         case "required":
-          if (this.doc[element][index]["required"] === false && input.value !== "") {
+          if (this.doc.forms[element][index]["required"] === false && input.value !== "") {
             input.required = false;
-            this.inputCheckFill(input, "", false);
-          } else if (this.doc[element][index]["required"] === true && input.value === ""){
-            input.required = true;
-            this.inputCheckFill(input, "Please fill in this field", true); 
+          } else if (this.doc.forms[element][index]["required"] === true && input.value === ""){
+            input.required = true; 
+           if(messageCheck === false) message = "Please fill in this field";
           }
           break;
         case "minLength":
         case "maxLength":
-          if (this.doc[element][index]["minLength"] > 0 && input.value.length < this.doc[element][index]["minLength"] && this.doc[element][index]["minLength"] !== undefined){
-            this.inputCheckFill(input, `Length of text less than ${this.doc[element][index]["minLength"]}`, true); 
-          } else if(input.value.length > this.doc[element][index]["maxLength"] && this.doc[element][index]["maxLength"] !== undefined) {
-            this.inputCheckFill(input, `Length of text larger than ${this.doc[element][index]["maxLength"]}`, true); 
+          if (this.doc.forms[element][index]["minLength"] > 0 && input.value.length < this.doc.forms[element][index]["minLength"] && this.doc.forms[element][index]["minLength"] !== undefined && inputType !== "date"){
+            if(messageCheck === false) message = `Length of text less than ${this.doc.forms[element][index]["minLength"]}`;          
+          } else if(input.value.length > this.doc.forms[element][index]["maxLength"] && this.doc.forms[element][index]["maxLength"] !== undefined && inputType !== "date") {
+            if(messageCheck === false) message = `Length of text larger than ${this.doc.forms[element][index]["maxLength"]}`;
           }
           break;
         case "emailValidation":
-          if (this.doc[element][index]["emailValidation"] === true && inputType === "email"){
+          if (this.doc.forms[element][index]["emailValidation"] === true && inputType === "email"){
             let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
             
             if (!filter.test(input.value)) {
-              this.inputCheckFill(input, "Email not written in proper way, please use following notation: user@example.com", true);
+              if(messageCheck === false) message = "Email not written in proper way, please use following notation: user@example.com";
             } 
           }
           break;
         case "checkWriting":
-          if (this.doc[element][index]["checkWriting"] === true && inputType === "password"){
+          if (this.doc.forms[element][index]["checkWriting"] === true && inputType === "password"){
             let filter = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/;
             
             if (!filter.test(input.value)) {
-              this.inputCheckFill(input, "Password not written in proper way, please use following notation: Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character", true);
+              if(messageCheck === false) message = "Password not written in proper way, please use following notation: Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character";
             } 
           }
           break;
         case "compareWith":
-          if (this.doc[element][index]["compareWith"].length > 0 && inputType === "password" && input.value !== null){
-            let el = document.getElementById(`#${this.doc[element][index]["compareWith"]}`);
+          if (this.doc.forms[element][index]["compareWith"].length > 0 && inputType === "password" && input.value !== null){
+            let id = "#" + this.doc.forms[element][index]["compareWith"];
+            let el = document.querySelector(id);
             
-            if (el !== input.value && el !== null || el === undefined) {
-              this.inputCheckFill(input, "Password doesn\'t match. Please check password again", true);
-            } 
+            if (el.value !== input.value && el !== null || el === undefined) {
+              if(messageCheck === false) message = "Password doesn\'t match. Please check password again";
+            } else if(el === null) message = "Password is impropper.";
+          }
+          break;
+        case "minVal":
+        case "maxVal":
+          if (inputType === "number" && this.doc.forms[element][index]["minVal"] === true && input.value !== null){
+            if(inputType === "number") input.min = this.doc.forms[element][index]["minVal"];
+            if(messageCheck === false && input.value < this.doc.forms[element][index]["maxVal"]) message = `Number mustn\'t be under ${this.doc.forms[element][index]["minVal"]}`;
+          } else if (inputType === "number" && this.doc.forms[element][index]["maxVal"] === true && input.value !== null) {
+            if(inputType === "number") input.max = this.doc.forms[element][index]["maxVal"];
+            if(messageCheck === false && input.value > this.doc.forms[element][index]["maxVal"]) message = `Number mustn\'t be above ${this.doc.forms[element][index]["maxVal"]}`;
           }
           break;
       }
+      if(message !== "") messageCheck = true;
     }
+    (messageCheck === true) ? this.inputCheckFill(input, message, true) : this.inputCheckFill(input, message, false);
   }
   inputCheckFill(inp, message, check) {
+    let notificationContainer = inp.parentNode.children;
     if (check) {
-      inp.classList.remove("input--ok");
-      inp.classList.add("input--err");
-      inp.parentNode.children[1].style.color = "red";
-      inp.setCustomValidity(message)
+      inp.classList.remove(this.okClass);
+      inp.classList.add(this.errClass);
+      inp.parentNode.children[2].style.color = "red";
+      notificationContainer[1].innerHTML = message; 
       inp.focus();
     } else {
-      inp.classList.add("input--ok");
-      inp.classList.remove("input--err");
-      inp.parentNode.children[1].style.color = "#26a69a";
+      inp.classList.add(this.okClass);
+      inp.classList.remove(this.errClass);
+      inp.parentNode.children[2].style.color = "#26a69a";
+      notificationContainer[1].innerHTML = "";
+    }
+  }
+  keyPressControl(element) {
+    if (this.inputs.length > 0 && this.ids !== undefined) {
+      this.ids.forEach((id, ind) => {
+        this.inputs[ind].addEventListener("keyup", () => {
+          let inputKeys = Object.getOwnPropertyNames(this.doc.forms[element][id]);
+          this.checkKeys(this.inputs[ind], inputKeys, element, id);
+        })
+      })
     }
   }
 }
 
 document.querySelector("#submitted").addEventListener("click", () => {
   new BanjiValidate({
-    "regular": {
-      "firstName" : {
-        "required": false,
-        "minLength": 6,
-        "maxLength": 30
-      },
-      "lastName" : {
-        "required": true,
-        "minLength": 4,
-        
-      },
-      "email" : {
-        "required": true,
-        "emailValidation": true
-      },
-      "password" : {
-        "required": true,
-        "minLength": 8,
-        "checkWriting": true
-      },
-      "confirmPassword" : {
-        "required": true,
-        "minLength": 8,
-        "compareWith": "password"
+    "forms": {
+      "regular": {
+        "firstName" : {
+          "required": true,
+          "minLength": 6,
+          "maxLength": 30
+        },
+        "lastName" : {
+          "required": true,
+          "minLength": 4,
+          
+        },
+        "email" : {
+          "required": true,
+          "emailValidation": true
+        },
+        "password" : {
+          "required": true,
+          "minLength": 8,
+          "checkWriting": true
+        },
+        "confirmPassword" : {
+          "required": true,
+          "minLength": 8,
+          "compareWith": "password"
+        },
+        "date" : {
+          "required": false
+        },
+        "numbers" : {
+          "required": true,
+          "minVal": 1,
+          "maxVal": 15
+        }
       }
-    }
+    },
+    "options": {
+      "customClassOk": "input--ok-custom",
+      "customClassError": "input--err-custom"
+    }    
   })
 })
