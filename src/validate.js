f5713585198b6a752576956de2255256643e7ba6
banjac275 <banjac275@gmail.com>
2018-08-10 16:04:15 +0200

    all done but there are some bugs

--- src/validate.js
+++ src/validate.js
@@ -25,15 +25,17 @@ class BanjiValidate {
       }
     })
     let options = Object.getOwnPropertyNames(this.doc.options);
-    if(options.length > 0 || options !== null) {
+    if(options.length > 0 || options !== null || options !== undefined) {
       options.forEach(el => {
         switch(el) {
           case "customClassOk":
             if (this.doc.options["customClassOk"].length > 0){
               this.okClass = this.doc.options["customClassOk"];
               this.inputs.forEach(inp => {
-                inp.classList.remove(this.okClass);
-                inp.classList.add(this.errClass);
+                if (inp.classList.contains("input--ok")) {
+                  inp.classList.remove("input--ok");
+                  inp.classList.add(this.okClass);
+                }
               })
             }
             break;
@@ -41,8 +43,10 @@ class BanjiValidate {
             if (this.doc.options["customClassError"].length > 0){
               this.errClass = this.doc.options["customClassError"];
               this.inputs.forEach(inp => {
-                inp.classList.add(this.okClass);
-                inp.classList.remove(this.errClass);
+                if (inp.classList.contains("input--err")) {
+                  inp.classList.remove("input--err");
+                  inp.classList.add(this.errClass);
+                }
               })
             } 
             break;
@@ -55,61 +59,60 @@ class BanjiValidate {
     let messageCheck = false;
     let inputType = input.type;
     for(let key in keys) {
-      switch(keys[key]) {
-        case "required":
-          if (this.doc.forms[element][index]["required"] === false && input.value !== "") {
-            input.required = false;
-          } else if (this.doc.forms[element][index]["required"] === true && input.value === ""){
-            input.required = true; 
-           if(messageCheck === false) message = "Please fill in this field";
-          }
-          break;
-        case "minLength":
-        case "maxLength":
-          if (this.doc.forms[element][index]["minLength"] > 0 && input.value.length < this.doc.forms[element][index]["minLength"] && this.doc.forms[element][index]["minLength"] !== undefined && inputType !== "date"){
-            if(messageCheck === false) message = `Length of text less than ${this.doc.forms[element][index]["minLength"]}`;          
-          } else if(input.value.length > this.doc.forms[element][index]["maxLength"] && this.doc.forms[element][index]["maxLength"] !== undefined && inputType !== "date") {
-            if(messageCheck === false) message = `Length of text larger than ${this.doc.forms[element][index]["maxLength"]}`;
-          }
-          break;
-        case "emailValidation":
-          if (this.doc.forms[element][index]["emailValidation"] === true && inputType === "email"){
-            let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-            
-            if (!filter.test(input.value)) {
-              if(messageCheck === false) message = "Email not written in proper way, please use following notation: user@example.com";
-            } 
-          }
-          break;
-        case "checkWriting":
-          if (this.doc.forms[element][index]["checkWriting"] === true && inputType === "password"){
-            let filter = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/;
-            
-            if (!filter.test(input.value)) {
-              if(messageCheck === false) message = "Password not written in proper way, please use following notation: Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character";
-            } 
-          }
-          break;
-        case "compareWith":
-          if (this.doc.forms[element][index]["compareWith"].length > 0 && inputType === "password" && input.value !== null){
-            let id = "#" + this.doc.forms[element][index]["compareWith"];
-            let el = document.querySelector(id);
-            
-            if (el.value !== input.value && el !== null || el === undefined) {
-              if(messageCheck === false) message = "Password doesn\'t match. Please check password again";
-            } else if(el === null) message = "Password is impropper.";
-          }
-          break;
-        case "minVal":
-        case "maxVal":
-          if (inputType === "number" && this.doc.forms[element][index]["minVal"] === true && input.value !== null){
-            if(inputType === "number") input.min = this.doc.forms[element][index]["minVal"];
-            if(messageCheck === false && input.value < this.doc.forms[element][index]["maxVal"]) message = `Number mustn\'t be under ${this.doc.forms[element][index]["minVal"]}`;
-          } else if (inputType === "number" && this.doc.forms[element][index]["maxVal"] === true && input.value !== null) {
-            if(inputType === "number") input.max = this.doc.forms[element][index]["maxVal"];
-            if(messageCheck === false && input.value > this.doc.forms[element][index]["maxVal"]) message = `Number mustn\'t be above ${this.doc.forms[element][index]["maxVal"]}`;
-          }
-          break;
+      if(input.value !== null) {
+        switch(keys[key]) {
+          case "required":
+            if (this.doc.forms[element][index]["required"] === false && input.value !== "") {
+              input.required = false;
+            } else if (this.doc.forms[element][index]["required"] === true && input.value === ""){
+              input.required = true; 
+            if(messageCheck === false) message = "Please fill this field";
+            }
+            break;
+          case "minLength":
+          case "maxLength":
+            if (this.doc.forms[element][index]["minLength"] > 0 && input.value.length < this.doc.forms[element][index]["minLength"] && this.doc.forms[element][index]["minLength"] !== undefined && inputType !== "date"){
+              if(messageCheck === false) message = `Length of text less than ${this.doc.forms[element][index]["minLength"]}`;          
+            } else if(input.value.length > this.doc.forms[element][index]["maxLength"] && this.doc.forms[element][index]["maxLength"] !== undefined && inputType !== "date") {
+              if(messageCheck === false) message = `Length of text larger than ${this.doc.forms[element][index]["maxLength"]}`;
+            }
+            break;
+          case "emailValidation":
+            if (this.doc.forms[element][index]["emailValidation"] === true && inputType === "email"){
+              let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+              
+              if (!filter.test(input.value)) {
+                if(messageCheck === false) message = "Email not written in proper way, please use following notation: user@example.com";
+              } 
+            }
+            break;
+          case "checkWriting":
+            if (this.doc.forms[element][index]["checkWriting"] === true && inputType === "password"){
+              let filter = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/;
+              
+              if (!filter.test(input.value)) {
+                if(messageCheck === false) message = "Password not written in proper way, please use following notation: " +
+                "Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character";
+              } 
+            }
+            break;
+          case "compareWith":
+            if (this.doc.forms[element][index]["compareWith"].length > 0 && inputType === "password"){
+              let id = "#" + this.doc.forms[element][index]["compareWith"];
+              let el = document.querySelector(id);
+              
+              if (el.value !== input.value && el !== null || el === undefined) {
+                if(messageCheck === false) message = "Password doesn\'t match. Please check password again";
+              } else if(el === null) message = "Password is impropper.";
+            }
+            break;
+          case "minVal":
+          case "maxVal":
+            if(input.value < this.doc.forms[element][index]["minVal"] && inputType === "number") message = `Number mustn\'t be under ${this.doc.forms[element][index]["minVal"]}`;
+            else if(input.value > this.doc.forms[element][index]["maxVal"] && inputType === "number") message = `Number mustn\'t be above ${this.doc.forms[element][index]["maxVal"]}`;
+            else message = "";
+            break;
+        }
       }
       if(message !== "") messageCheck = true;
     }
@@ -133,6 +136,12 @@ class BanjiValidate {
   keyPressControl(element) {
     if (this.inputs.length > 0 && this.ids !== undefined) {
       this.ids.forEach((id, ind) => {
+        if(this.inputs[ind].type === "number") {
+          this.inputs[ind].addEventListener("change", () => {
+            let inputKeys = Object.getOwnPropertyNames(this.doc.forms[element][id]);
+            this.checkKeys(this.inputs[ind], inputKeys, element, id);
+          })
+        } 
         this.inputs[ind].addEventListener("keyup", () => {
           let inputKeys = Object.getOwnPropertyNames(this.doc.forms[element][id]);
           this.checkKeys(this.inputs[ind], inputKeys, element, id);
